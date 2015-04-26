cbdf10f059a333cc2775b6c68d80a5fae7a93c2a
Orbán István <firstvan@live.com>
2015-04-26 16:46:06 +0200

    Create CustomerTest

---
+++ src/test/java/hu/firstvan/model/CustomerTest.java
@@ -0,0 +1,70 @@
+package hu.firstvan.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by firstvan on 2015.04.26..
+ */
+public class CustomerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetC_id() throws Exception {
+
+    }
+
+    @Test
+    public void testSetC_id() throws Exception {
+
+    }
+
+    @Test
+    public void testGetC_name() throws Exception {
+
+    }
+
+    @Test
+    public void testSetC_name() throws Exception {
+
+    }
+
+    @Test
+    public void testGetC_startDate() throws Exception {
+
+    }
+
+    @Test
+    public void testSetC_startDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetC_addr() throws Exception {
+
+    }
+
+    @Test
+    public void testSetC_addr() throws Exception {
+
+    }
+
+    @Test
+    public void testGetC_firstOrder() throws Exception {
+
+    }
+
+    @Test
+    public void testSetC_firstOrder() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+}
