461591f64f1347a704a49f925edec3947e886831
Orbán István <firstvan@live.com>
2015-04-27 16:37:18 +0200

    Change structure of DatabaseStage class to better coverage.

--- src/main/java/hu/firstvan/view/DatabaseStage.java
+++ src/main/java/hu/firstvan/view/DatabaseStage.java
@@ -5,18 +5,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
  * Created by firstvan on 2015.04.14..
  */
 public class DatabaseStage {
 
+    public static Stage stage;
 
-    public DatabaseStage() throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+    public DatabaseStage() throws Exception{
+        Parent root = FXMLLoader.load(DatabaseStage.class.getResource("/fxml/Scene.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        Stage stage = new Stage();
+        stage = new Stage();
         stage.setTitle("Database connection");
         stage.setScene(scene);
         stage.show();
