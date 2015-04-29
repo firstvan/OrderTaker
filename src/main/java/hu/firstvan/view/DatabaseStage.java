76c006a22dcca92305536e4db16e0c424f842c93
firstvan <firstvan@live.com>
2015-04-29 10:08:52 +0200

    Info log put into database stage class.

--- src/main/java/hu/firstvan/view/DatabaseStage.java
+++ src/main/java/hu/firstvan/view/DatabaseStage.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -14,14 +16,17 @@ public class DatabaseStage {
 
     public static Stage stage;
 
+    private static Logger logger = LoggerFactory.getLogger(MainApp.class);
+
     public DatabaseStage() throws Exception{
         Parent root = FXMLLoader.load(DatabaseStage.class.getResource("/fxml/Scene.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         stage = new Stage();
-        stage.setTitle("Database connection");
+        stage.setTitle("Adatbázis kapcsolódás");
         stage.setScene(scene);
         stage.show();
+        logger.info("Database stage display.");
     }
 }
