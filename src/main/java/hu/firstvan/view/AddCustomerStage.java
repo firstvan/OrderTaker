5f4c5e00c410ed26ed674bbaa03e720030c6aa46
firstvan <firstvan@live.com>
2015-04-29 10:08:01 +0200

    Info log put.

--- src/main/java/hu/firstvan/view/AddCustomerStage.java
+++ src/main/java/hu/firstvan/view/AddCustomerStage.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by firstvan on 2015.04.18..
@@ -11,14 +13,18 @@ import javafx.stage.Stage;
 public class AddCustomerStage {
     public static Stage stage = new Stage();
 
+    private static Logger logger = LoggerFactory.getLogger(AddCustomerStage.class);
+
+
     public AddCustomerStage() throws Exception {
         Parent root = FXMLLoader.load(AddCustomerStage.class.getResource("/fxml/AddCustomer.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
 
-        stage.setTitle("Database connection");
+        stage.setTitle("Vásárló hozzáadás");
         stage.setScene(scene);
         stage.show();
+        logger.info("Add customer stage is opened.");
     }
 }
