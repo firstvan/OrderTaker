06d08071a2f2c169e81fe36d6b1a8ac2adc68e11
firstvan <firstvan@live.com>
2015-05-05 22:24:57 +0200

    Added javadoc for test.

--- src/test/java/hu/firstvan/model/ProductsTest.java
+++ src/test/java/hu/firstvan/model/ProductsTest.java
@@ -6,170 +6,258 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by firstvan on 2015.04.25..
+ * This class represent test of product class.
  */
 public class ProductsTest {
 
-    Products products;
+  /**
+   * It's contains all necessary information of product.
+   */
+  private Products products;
 
-    @Before
-    public void setUp() throws Exception {
-        products = new Products(12345, "Termek", 200, "Van", 10, 100, 11, 90, 12, 80);
-        products.setOrderdPiece(1);
-    }
+  /**
+   * Create a new product before test.
+   */
+  @Before
+  public void setUp() {
+    products = new Products(12345, "Termek", 200, "Van", 10, 100, 11, 90, 12, 80);
+    products.setOrderdPiece(1);
+  }
 
-    @Test
-    public void testSetItemNo() throws Exception {
-        products.setItemNo(1234512);
-        assertEquals((Integer) 1234512, products.getItemNo());
-    }
+  /**
+   * Test setter of product's item number.
+   */
+  @Test
+  public void testSetItemNo() {
+    products.setItemNo(1234512);
+    assertEquals((Integer) 1234512, products.getItemNo());
+  }
 
-    @Test
-    public void testGetItemNo() throws Exception {
-        assertEquals((Integer) 12345, products.getItemNo());
-    }
+  /**
+   * Test getter of product's item number.
+   */
+  @Test
+  public void testGetItemNo() {
+    assertEquals((Integer) 12345, products.getItemNo());
+  }
+
+  /**
+   * Test getter of product's name.
+   */
+  @Test
+  public void testGetName() {
+    assertEquals("Termek", products.getName());
+  }
+
+  /**
+   * Test setter of product's name.
+   */
+  @Test
+  public void testSetName() {
+    products.setName("Hello");
+    assertEquals("Hello", products.getName());
+  }
+
+  /**
+   * Test getter of product's price.
+   */
+  @Test
+  public void testGetPrice() {
+    assertEquals((Integer) 200, products.getPrice());
+  }
+
+  /**
+   * Test setter of product's price.
+   */
+  @Test
+  public void testSetPrice() {
+    products.setPrice(100);
+    assertEquals((Integer) 100, products.getPrice());
+  }
 
 
-    @Test
-    public void testGetName() throws Exception {
-        assertEquals("Termek", products.getName());
-    }
+  /**
+   * Test getter whether product has salary.
+   */
+  @Test
+  public void testGetRabat() {
+    assertEquals("Van", products.getRabat());
+  }
 
-    @Test
-    public void testSetName() throws Exception {
-        products.setName("Hello");
-        assertEquals("Hello", products.getName());
-    }
+  /**
+   * Test setter of whether product has salary.
+   */
+  @Test
+  public void testSetRabat() {
+    products.setRabat("Nincs");
+    assertEquals("Nincs", products.getRabat());
+  }
 
-    @Test
-    public void testGetPrice() throws Exception {
-        assertEquals((Integer) 200, products.getPrice());
-    }
+  /**
+   * Test getter of product's first salary.
+   */
+  @Test
+  public void testGetRabat1() {
+    assertEquals((Integer) 10, products.getRabat1());
+  }
 
-    @Test
-    public void testSetPrice() throws Exception {
-        products.setPrice(100);
-        assertEquals((Integer) 100, products.getPrice());
-    }
+  /**
+   * Test setter of product's salary piece.
+   */
+  @Test
+  public void testSetRabat1() {
+    products.setRabat1(5);
+    assertEquals((Integer) 5, products.getRabat1());
+  }
 
-    @Test
-    public void testGetRabat() throws Exception {
-        assertEquals("Van", products.getRabat());
-    }
+  /**
+   * Test getter of product's first salary price.
+   */
+  @Test
+  public void testGetRabat1Price() {
+    assertEquals((Integer) 100, products.getRabat1Price());
+  }
 
-    @Test
-    public void testSetRabat() throws Exception {
-        products.setRabat("Nincs");
-        assertEquals("Nincs", products.getRabat());
-    }
+  /**
+   * Test setter of product's salary.
+   */
+  @Test
+  public void testSetRabat1Price() {
+    products.setRabat1Price(123);
+    assertEquals((Integer) 123, products.getRabat1Price());
+  }
 
-    @Test
-    public void testGetRabat1() throws Exception {
-        assertEquals((Integer) 10, products.getRabat1());
-    }
+  /**
+   * Test getter of product's second salary piece.
+   */
+  @Test
+  public void testGetRabat2() {
+    assertEquals((Integer) 11, products.getRabat2());
+  }
 
-    @Test
-    public void testSetRabat1() throws Exception {
-        products.setRabat1(5);
-        assertEquals((Integer) 5, products.getRabat1());
-    }
 
-    @Test
-    public void testGetRabat1Price() throws Exception {
-        assertEquals((Integer) 100, products.getRabat1Price());
-    }
+  /**
+   * Test setter of product's salary 2 piece.
+   */
+  @Test
+  public void testSetRabat2() {
+    products.setRabat2(5);
+    assertEquals((Integer) 5, products.getRabat2());
 
-    @Test
-    public void testSetRabat1Price() throws Exception {
-        products.setRabat1Price(123);
-        assertEquals((Integer) 123, products.getRabat1Price());
-    }
+  }
 
-    @Test
-    public void testGetRabat2() throws Exception {
-        assertEquals((Integer) 11, products.getRabat2());
-    }
+  /**
+   * Test getter of product's second salary price.
+   */
+  @Test
+  public void testGetRabat2Price() {
+    assertEquals((Integer) 90, products.getRabat2Price());
+  }
 
-    @Test
-    public void testGetSubTotal3() throws Exception {
-        products.setOrderdPiece(12);
-        assertEquals((Integer) 960, products.getSubTotal());
-    }
+  /**
+   * Test setter of product's salary 2.
+   */
+  @Test
+  public void testSetRabat2Price() {
+    products.setRabat2Price(123);
+    assertEquals((Integer) 123, products.getRabat2Price());
+  }
 
-    @Test
-    public void testSetRabat2() throws Exception {
-        products.setRabat2(5);
-        assertEquals((Integer) 5, products.getRabat2());
+  /**
+   * Test getter of product's third salary piece.
+   */
+  @Test
+  public void testGetRabat3() {
+    assertEquals((Integer) 12, products.getRabat3());
+  }
 
-    }
+  /**
+   * Test setter of product's salary 3 piece .
+   */
+  @Test
+  public void testSetRabat3() {
+    products.setRabat3(5);
+    assertEquals((Integer) 5, products.getRabat3());
 
-    @Test
-    public void testGetRabat2Price() throws Exception {
-        assertEquals((Integer) 90, products.getRabat2Price());
-    }
+  }
 
-    @Test
-    public void testSetRabat2Price() throws Exception {
-        products.setRabat2Price(123);
-        assertEquals((Integer) 123, products.getRabat2Price());
-    }
+  /**
+   * Test getter of product's third salary price.
+   */
+  @Test
+  public void testGetRabat3Price() {
+    assertEquals((Integer) 80, products.getRabat3Price());
+  }
 
-    @Test
-    public void testGetRabat3() throws Exception {
-        assertEquals((Integer) 12, products.getRabat3());
-    }
+  /**
+   * Test setter of product's salary 3.
+   */
+  @Test
+  public void testSetRabat3Price() {
+    products.setRabat3Price(123);
+    assertEquals((Integer) 123, products.getRabat3Price());
+  }
 
-    @Test
-    public void testSetRabat3() throws Exception {
-        products.setRabat3(5);
-        assertEquals((Integer) 5, products.getRabat3());
+  /**
+   * Test getter of ordered price.
+   */
+  @Test
+  public void testGetOrderdPiece() {
+    assertEquals((Integer) 1, products.getOrderdPiece());
+  }
 
-    }
+  /**
+   * Test setter of product's ordered piece.
+   */
+  @Test
+  public void testSetOrderdPiece() {
+    products.setOrderdPiece(12);
+    assertEquals((Integer) 12, products.getOrderdPiece());
+  }
 
-    @Test
-    public void testGetRabat3Price() throws Exception {
-        assertEquals((Integer) 80, products.getRabat3Price());
-    }
+  /**
+   * Test sub total when no salary.
+   */
+  @Test
+  public void testGetSubTotal() {
+    assertEquals((Integer) 200, products.getSubTotal());
+  }
 
-    @Test
-    public void testSetRabat3Price() throws Exception {
-        products.setRabat3Price(123);
-        assertEquals((Integer) 123, products.getRabat3Price());
-    }
+  /**
+   * Test sub total when first salary is active.
+   */
+  @Test
+  public void testGetSubTotal1() {
+    products.setOrderdPiece(10);
+    assertEquals((Integer) 1000, products.getSubTotal());
+  }
 
-    @Test
-    public void testGetOrderdPiece() throws Exception {
-        assertEquals((Integer) 1, products.getOrderdPiece());
-    }
+  /**
+   * Test sub total when second salary is active.
+   */
+  @Test
+  public void testGetSubTotal2() {
+    products.setOrderdPiece(11);
+    assertEquals((Integer) 990, products.getSubTotal());
+  }
 
-    @Test
-    public void testSetOrderdPiece() throws Exception {
-        products.setOrderdPiece(12);
-        assertEquals((Integer) 12, products.getOrderdPiece());
-    }
+  /**
+   * Test sub total when third salary is active.
+   */
+  @Test
+  public void testGetSubTotal3() {
+    products.setOrderdPiece(12);
+    assertEquals((Integer) 960, products.getSubTotal());
+  }
 
-    @Test
-    public void testGetSubTotal() throws Exception {
-        assertEquals((Integer) 200, products.getSubTotal());
-    }
+  /**
+   * Test two products equals.
+   */
+  @Test
+  public void testEquals() {
+    Products p = new Products();
+    p.setItemNo(12345);
 
-    @Test
-    public void testGetSubTotal1() throws Exception {
-        products.setOrderdPiece(10);
-        assertEquals((Integer) 1000, products.getSubTotal());
-    }
-
-    @Test
-    public void testGetSubTotal2() throws Exception {
-        products.setOrderdPiece(11);
-        assertEquals((Integer) 990, products.getSubTotal());
-    }
-
-    @Test
-    public void testEquals() throws Exception {
-        Products p = new Products();
-        p.setItemNo(12345);
-
-        assertEquals(true, products.equals(p));
-    }
+    assertEquals(true, products.equals(p));
+  }
 }
