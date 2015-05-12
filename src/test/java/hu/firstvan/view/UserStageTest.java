4467787a23b3ad0ad152a0b37965c161c10f9807
Orbán István <firstvan@live.com>
2015-05-12 18:21:47 +0200

    Clear the repository

--- src/test/java/hu/firstvan/view/UserStageTest.java
+++ src/test/java/hu/firstvan/view/UserStageTest.java
@@ -5,37 +5,24 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-/**
- * Created by firstvan on 2015.04.26..
- */
 public class UserStageTest {
     @Rule
     public JavaFXThreadingRule jfxRule = new JavaFXThreadingRule();
 
     @Before
     public void setUp() throws Exception {
-<<<<<<< HEAD
         new CustomerStage();
-=======
-        new UserStage();
->>>>>>> 9642e09e97c72de16cc3df97f83d98647eb471ce
     }
 
     @Test
     public void testNotNull() {
-<<<<<<< HEAD
         TestCase.assertNotNull(CustomerStage.stage);
-=======
-        TestCase.assertNotNull(UserStage.stage);
->>>>>>> 9642e09e97c72de16cc3df97f83d98647eb471ce
+        TestCase.assertNotNull(CustomerStage.stage);
     }
 
     @Test
     public void testControllerNotNull() {
-<<<<<<< HEAD
         TestCase.assertNotNull(CustomerStage.getController());
-=======
-        TestCase.assertNotNull(UserStage.getController());
->>>>>>> 9642e09e97c72de16cc3df97f83d98647eb471ce
+        TestCase.assertNotNull(CustomerStage.getController());
     }
 }
