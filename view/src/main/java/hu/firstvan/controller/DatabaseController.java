91d7c2c7cf6eb97697bb5c2ff0e92f44d32c7c55
Orbán István <firstvan@live.com>
2015-05-12 19:39:06 +0200

    Added to view, becouse circural dependecies, can not be two modul view and controller

---
+++ view/src/main/java/hu/firstvan/controller/DatabaseController.java
@@ -0,0 +1,105 @@
+package hu.firstvan.controller;
+
+import hu.firstvan.model.ConnectionFactory;
+import hu.firstvan.view.MainStage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+/**
+ * Controll the Database sign in stage.
+ */
+public class DatabaseController implements Initializable {
+
+    /**
+     * Database user name.
+     */
+    @FXML
+    private javafx.scene.control.TextField jdbc_user;
+
+    /**
+     * Database user password.
+     */
+    @FXML
+    private javafx.scene.control.TextField jdbc_pass;
+
+    /**
+     *  Set's static database user information, and open Main window.
+     *
+     * @param event default param in javafx controller.
+     * @throws Exception when the {@code mainstage.fxml} could not found.
+     */
+    @FXML
+    public void LogInAction(ActionEvent event) throws Exception {
+
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+        ConnectionFactory.setUser(jdbc_user.getText());
+        ConnectionFactory.setPass(jdbc_pass.getText());
+        new MainStage();
+
+    }
+
+    /**
+     * Exit when x button is clicked.
+     *
+     * @param event x button is clicked
+     */
+    @FXML
+    public void ExitAction(ActionEvent event) {
+        System.exit(1);
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param url  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param rb The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @FXML
+    private Button jdbc_login;
+
+    /**
+     * Check user and password field is not empty.
+     *
+     * @param event get a keyevent
+     */
+    @FXML
+    public void userField(KeyEvent event) {
+
+        if (jdbc_user.getText().length() >= 0 && jdbc_pass.getText().length() > 0) {
+            jdbc_login.setDisable(false);
+        } else {
+            jdbc_login.setDisable(true);
+        }
+    }
+
+    /**
+     * Check user and password field is not empty.
+     *
+     * @param event get a keyevent
+     */
+    @FXML
+    public void passwordField(KeyEvent event) {
+        if (jdbc_user.getText().length() > 0 && jdbc_pass.getText().length() >= 0) {
+            jdbc_login.setDisable(false);
+        } else {
+            jdbc_login.setDisable(true);
+        }
+    }
+}
---
+++ view/src/main/java/hu/firstvan/controller/MainController.java
@@ -0,0 +1,147 @@
+package hu.firstvan.controller;
+
+import hu.firstvan.model.DatabaseDAO;
+import hu.firstvan.model.Datas;
+import hu.firstvan.model.Orders;
+import hu.firstvan.view.CustomerStage;
+import hu.firstvan.view.MainStage;
+import hu.firstvan.view.ProductStage;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+/**
+ * Manage all interactions on {@code Main Window}.
+ */
+public class MainController implements Initializable {
+
+    /**
+     * Table for show all orders.
+     */
+    @FXML
+    private TableView<Orders> ordered;
+
+    /**
+     * First column of table. It's order id.
+     */
+    @FXML
+    private TableColumn<Orders, Integer> o_id;
+
+    /**
+     * Second column of table. It's order date.
+     */
+    @FXML
+    private TableColumn<Orders, Date> o_date;
+
+    /**
+     * Third column of table. It's order price.
+     */
+    @FXML
+    private TableColumn<Orders, Integer> o_prices;
+
+    /**
+     * Add new order button.
+     */
+    @FXML
+    private Button addOrder;
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        o_id.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().getO_id()));
+        o_date.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().getDate()));
+        o_prices.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().getOrder_total()));
+    }
+
+    /**
+     * Open customer chooser dialog.
+     */
+    @FXML
+    private void newOrderTake() {
+        try {
+            new CustomerStage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error dialog");
+            alert.setHeaderText("Hiba a vásárlók betöltésénél.");
+            alert.show();
+        }
+    }
+
+    /**
+     * Label for customer name.
+     */
+    @FXML
+    private Label custName;
+
+    /**
+     * Set's customer name.
+     *
+     * @param s customer's name
+     */
+    public void setLabel(String s) {
+        custName.setText(s);
+    }
+
+    /**
+     * Set's ordered table with selected customer's orders.
+     */
+    public void changeTable() {
+        DatabaseDAO databaseDAO = new DatabaseDAO();
+        ArrayList<Orders> orders = databaseDAO.GetUserOrders(Datas.getCustomer().getC_id());
+        if (!orders.isEmpty()) {
+            ObservableList<Orders> customerData = FXCollections.observableArrayList(orders);
+            ordered.setItems(customerData);
+        }
+    }
+
+    /**
+     * Open the stage for add new order.
+     */
+    @FXML
+    public void addOrder() {
+        new ProductStage();
+        MainStage.stage.close();
+    }
+
+    /**
+     * Close the program.
+     */
+    @FXML
+    public void close() {
+        System.exit(0);
+    }
+
+    /**
+     * Enable {@code addOrder} button, after customer is selected.
+     */
+    public void enableButton() {
+        addOrder.setDisable(false);
+    }
+
+    /**
+     * Exit from the menu.
+     *
+     * @param actionEvent an event.
+     */
+    public void Exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
