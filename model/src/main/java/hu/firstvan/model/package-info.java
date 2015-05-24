6d05b161d585c97a7a336d6dd6ab8ae2a6c49e15
Orban Istvan <firstvan@live.com>
2015-05-24 09:35:01 +0200

    Added package info

---
+++ model/src/main/java/hu/firstvan/model/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the models and business logic.
+ *
+ * In this package there are connection factory to database, and dao to get data from there.
+ * Furthermore there are the models: {@code Customer } class is represent the customer.
+ * {@code Orders} class represent an order. {@code Products} class represent a product.
+ * {@code Datas} class contains the main information from the actual order like customer, ordered items.
+ */
+package hu.firstvan.model;
